66d59ac1832f59e7bf5a62b8cc6a4f8db53d46cb
Hilmi Yafi A <56539775+hilmiyafia@users.noreply.github.com>
2025-04-08 18:15:02 +0700

    tst

--- web3/flutter_bootstrap.js
+++ web3/flutter_bootstrap.js
@@ -11,7 +11,7 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
     serviceWorkerSettings: {
-        serviceWorkerVersion: "2497309583",
+        serviceWorkerVersion: "3629157773",
     },
     onEntrypointLoaded: async function (engineInitializer) {
         const appRunner = await engineInitializer.initializeEngine({useColorEmoji: useColorEmoji});
